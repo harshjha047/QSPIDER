6c8370e1ec86d9af2d3d68cf4354f840151fd1c7
harshjha047 <spyderhj11@gmail.com>
2025-06-17 09:45:53 +0530

    Form

---
+++ JavaScript/script.js
@@ -0,0 +1,29 @@
+// console.log("this is from external .js file");
+// console.log(10 + 2);
+// var hi = "hi";
+// console.log(hi);
+
+// var
+var a;
+console.log(a);
+a = 5;
+console.log(a);
+var a;
+console.log(a);
+var a = 10;
+console.log(a);
+//  let
+let b;
+console.log(b);
+b = 5;
+console.log(b);
+// let b;
+console.log(b);
+b = 10;
+console.log(b);
+// const 
+// const a;console.log(a)
+a = 5;console.log(a)
+// const a;console.log(a)
+a =10;console.log(a)
+
